077a2ac6f805e69b9f877119d916d6715c1c1edb
WildkrattFan <88059692+WildkrattFan@users.noreply.github.com>
2024-08-10 21:08:05 -0500

    working on asteroid splitting

--- scenees/asteroid.gd
+++ scenees/asteroid.gd
@@ -1,20 +1,24 @@
 extends Node2D
 
-var randomScale
+var asteroidScene = preload("res://scenees/asteroid.tscn")
+
 var health
-var randomDirection = Vector2.ZERO
 var randomSpeed
 var randomRotation
 var randomRotatation_speed
 
+var randomDirection = Vector2(randf_range(-1, 1), randf_range(-1, 1))
+
+var lower_size = 1
+var upper_size = 15
+var randomScale = randf_range(lower_size,upper_size)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	randomScale = randf_range(1,15)
 	scale.x = randomScale
 	scale.y = randomScale
 	health = randomScale * 10
-	randomDirection = Vector2(randf_range(-1, 1), randf_range(-1, 1))
 	randomSpeed = randf_range(0,3)
 	randomRotation = randf_range(0,359)
 	rotation = randomRotation
@@ -31,9 +35,7 @@ func _process(delta):
 func _on_hitbox_area_entered(area):
 
 	if area.is_in_group("Obstical"):
-		health -= 10
-		randomDirection = -randomDirection
-		randomRotatation_speed = -randomRotatation_speed
+		split(area)
 		
 	elif area.name == "pearcing_missile":
 		health -=10
@@ -56,5 +58,30 @@ func _on_hitbox_area_entered(area):
 	if area.name == "hitBox":
 		health -= 100
 
-func split():
-	pass
+func split(area):
+	var asteroid = area.get_parent()
+	var asteroid_size = asteroid.scale.x
+	
+	if asteroid_size > randomScale:
+		var split_count = ceil(asteroid_size / randomScale)
+		var split_lower_size = lower_size / split_count
+		var split_uper_size = upper_size / split_count
+		
+		for i in range(split_count):
+			var new_asteroid = asteroidScene.instantiate()
+
+			var new_scale = randf_range(split_lower_size,split_uper_size)
+			new_asteroid.scale = Vector2(new_scale, new_scale)
+			
+			var split_direction = -randomDirection 
+			
+			# Set the position slightly offset from the original
+			var offset = Vector2(randf_range(-10, 10), randf_range(-10, 10))
+			new_asteroid.position = position + offset
+		
+			get_parent().add_child(new_asteroid)
+			
+			queue_free()
+	else:
+		pass
+
